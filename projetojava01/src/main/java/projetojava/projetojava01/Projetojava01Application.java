2397b4a2c66c9fd197db0272cf7f20f9c0dddbbc
wellramosdev <well90dev@gmail.com>
2022-02-03 23:46:50 -0300

    projeto-java
    Estruturando projeto

---
+++ projetojava01/src/main/java/projetojava/projetojava01/Projetojava01Application.java
@@ -0,0 +1,13 @@
+package projetojava.projetojava01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Projetojava01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Projetojava01Application.class, args);
+	}
+
+}
---
+++ projetojava01/src/test/java/projetojava/projetojava01/Projetojava01ApplicationTests.java
@@ -0,0 +1,13 @@
+package projetojava.projetojava01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Projetojava01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
